52c311821051521b5ccda4a1f988603b00dcbe3d
Franz Sarmiento <franzks@gmail.com>
2013-11-19 02:47:23 +0800

    Added game icons in the game chooser screen.

--- war/js/homepage.js
+++ war/js/homepage.js
@@ -24,13 +24,24 @@ function gameIconClick(e) {
 function gameIconHoverIn(e) {
 	if (e.target.id == "game-icon-picture") {
 		$("#mascot-bubble").text($("#msg-picture").text());
+		$("#game-icon-picture").attr("src","img/picture_game_icon_hover.png");
 	} else if (e.target.id == "game-icon-search") {
 		$("#mascot-bubble").text($("#msg-search").text());
+		$("#game-icon-search").attr("src","img/search_game_icon_hover.png");
 	} else if (e.target.id == "game-icon-scramble") {
 		$("#mascot-bubble").text($("#msg-scramble").text());
+		$("#game-icon-scramble").attr("src","img/scramble_game_icon_hover.png");
 	}
 }
 
 function gameIconHoverOut(e) {
 	$("#mascot-bubble").text($("#msg-default").text());
+	
+	if (e.target.id == "game-icon-picture") {
+		$("#game-icon-picture").attr("src","img/picture_game_icon.png");
+	} else if (e.target.id == "game-icon-search") {
+		$("#game-icon-search").attr("src","img/search_game_icon.png");
+	} else if (e.target.id == "game-icon-scramble") {
+		$("#game-icon-scramble").attr("src","img/scramble_game_icon.png");
+	}
 }
