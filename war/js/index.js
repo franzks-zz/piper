6a038af84f775f8d5cdf7522c25bca7e94c175de
Franz Sarmiento <franzks@gmail.com>
2013-12-27 18:20:19 +0800

    Added logging in using Facebook
    Facebook login has been added, to obtain a bigger target audience.

--- war/js/index.js
+++ war/js/index.js
@@ -1,17 +1,19 @@
-$.ajaxSetup({ cache: false });
+$.ajaxSetup({
+	cache : false
+});
 
 var socialNetwork;
 
 $(function() {
 	socialNetwork = new SocialNetwork();
-	
+
 	$("#wrap-inner").load("landing.html");
 	$("#logo").click(logoClick);
 	$("#btn-sign-out").click(socialNetwork.btnSignOutClick);
 });
 
 function logoClick(e) {
-	document.location.href="/";
+	document.location.href = "/";
 }
 
 var GENDER_MALE = 1;
@@ -25,82 +27,91 @@ var gender;
 var mascotPose;
 
 function randomizeGender() {
-	gender = Math.floor(Math.random()*2+1);
+	gender = Math.floor(Math.random() * 2 + 1);
 }
 
 function swapMascot(gender, mascotPose) {
-	if(gender == GENDER_MALE) {
-		
-		if(mascotPose == MASCOT_REGULAR) {
-			$("#mascot-img").attr("src","img/mascot_boy.png");
-		} else if(mascotPose == MASCOT_SAD) {
-			$("#mascot-img").attr("src","img/mascot_boy_sad.png");
-		} else if(mascotPose == MASCOT_WIN) {
-			$("#mascot-img").attr("src","img/mascot_boy_win.png");
+	if (gender == GENDER_MALE) {
+
+		if (mascotPose == MASCOT_REGULAR) {
+			$("#mascot-img").attr("src", "img/mascot_boy.png");
+		} else if (mascotPose == MASCOT_SAD) {
+			$("#mascot-img").attr("src", "img/mascot_boy_sad.png");
+		} else if (mascotPose == MASCOT_WIN) {
+			$("#mascot-img").attr("src", "img/mascot_boy_win.png");
 		}
-		
-	} else if(gender == GENDER_FEMALE) {
-		
-		if(mascotPose == MASCOT_REGULAR) {
-			$("#mascot-img").attr("src","img/mascot_girl.png");
-		} else if(mascotPose == MASCOT_SAD) {
-			$("#mascot-img").attr("src","img/mascot_girl_sad.png");
-		} else if(mascotPose == MASCOT_WIN) {
-			$("#mascot-img").attr("src","img/mascot_girl_win.png");
+
+	} else if (gender == GENDER_FEMALE) {
+
+		if (mascotPose == MASCOT_REGULAR) {
+			$("#mascot-img").attr("src", "img/mascot_girl.png");
+		} else if (mascotPose == MASCOT_SAD) {
+			$("#mascot-img").attr("src", "img/mascot_girl_sad.png");
+		} else if (mascotPose == MASCOT_WIN) {
+			$("#mascot-img").attr("src", "img/mascot_girl_win.png");
 		}
-		
+
 	}
 }
 
 /*
- * 	Util Functions
- */ 
+ * Util Functions
+ */
 function shuffle(array) {
-    var counter = array.length, temp, index;
+	var counter = array.length, temp, index;
 
-    // While there are elements in the array
-    while (counter--) {
-        // Pick a random index
-        index = (Math.random() * counter) | 0;
+	// While there are elements in the array
+	while (counter--) {
+		// Pick a random index
+		index = (Math.random() * counter) | 0;
 
-        // And swap the last element with it
-        temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
-    }
+		// And swap the last element with it
+		temp = array[counter];
+		array[counter] = array[index];
+		array[index] = temp;
+	}
 
-    return array;
+	return array;
 }
 
 function preloadImages(arrayOfImages) {
-    $(arrayOfImages).each(function(){
-        $('<img/>')[0].src = this;
-    });
+	$(arrayOfImages).each(function() {
+		$('<img/>')[0].src = this;
+	});
 }
 
 $.cssHooks.backgroundColor = {
-    get: function(elem) {
-        if (elem.currentStyle)
-            var bg = elem.currentStyle["backgroundColor"];
-        else if (window.getComputedStyle)
-            var bg = document.defaultView.getComputedStyle(elem,
-                null).getPropertyValue("background-color");
-        if (bg.search("rgb") == -1)
-            return bg;
-        else {
-        	bg = bg.replace("rgba","rgb");
-        	if(bg.match(/,/g).length > 2) {        		
-        		bg = bg.substring(0,bg.lastIndexOf(','));
-        		bg += ')';
-        	}
-            bg = bg.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-            function hex(x) {
-                return ("0" + parseInt(x).toString(16)).slice(-2);
-            }
-            return "#" + hex(bg[1]) + hex(bg[2]) + hex(bg[3]);
-        }
-    }
+	get : function(elem) {
+		if (elem.currentStyle)
+			var bg = elem.currentStyle["backgroundColor"];
+		else if (window.getComputedStyle)
+			var bg = document.defaultView.getComputedStyle(elem, null)
+					.getPropertyValue("background-color");
+		if (bg.search("rgb") == -1)
+			return bg;
+		else {
+			bg = bg.replace("rgba", "rgb");
+			if (bg.match(/,/g).length > 2) {
+				bg = bg.substring(0, bg.lastIndexOf(','));
+				bg += ')';
+			}
+			bg = bg.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+			function hex(x) {
+				return ("0" + parseInt(x).toString(16)).slice(-2);
+			}
+			return "#" + hex(bg[1]) + hex(bg[2]) + hex(bg[3]);
+		}
+	}
 }
+
+/*
+ * Variables to check which social network was used for login
+ */
+
+var PLATFORM_GOOGLE = 1;
+var PLATFORM_FACEBOOK = 2;
+
+var platform = -1;
 
 /*
  * Object for handling all social-network related API calls
@@ -110,57 +121,22 @@ function SocialNetwork() {
 	this.justSignedOut = false;
 }
 
-SocialNetwork.prototype.signinCallback = function(authResult) {
-	if (authResult['access_token']) {		
-		$("#signin-wrapper").css("display", "none");
-		
-		gapi.client.load('plus', 'v1', function() {
-			var request = gapi.client.plus.people.list({
-				'userId' : 'me',
-				'collection' : 'visible',
-				'orderBy' : 'best'
-			});
-			request.execute(function(resp) {
-				if(resp.totalItems >= 10) {
-					$("#btn-play").css("display", "block");
-				} else {
-					$("#msg-error").css("display", "block");
-				}
-			});
-		});
-		
-		gapi.client.load('plus', 'v1', function() {
-			var request = gapi.client.plus.people.get({
-				'userId' : 'me'
-			});
-			request.execute(function(resp) {
-				socialNetwork.displayUser(resp);
-			});
-		});
-		
-		randomizeGender();
-	} else if (authResult['error']) {
-		
-		if(this.justSignedOut) {
-			document.location.href="/";
-		} else {
-			$("#signin-wrapper").css("display", "block");
-		}
-	}
-}
-
-SocialNetwork.prototype.displayUser = function(resp) {
-	var url = resp.image.url;
-	url = url.substring(0,url.length-2);
-	url += "30";
-	$("#nav-profile-photo").attr('src',url);
-	$("#welcome-message").text("Welcome, " + resp.displayName + "!");
+SocialNetwork.prototype.displayUser = function(displayName, url) {
+	$("#nav-profile-photo").attr('src', url);
+	$("#welcome-message").text("Welcome, " + displayName + "!");
 	$("#nav-profile").css('display', 'block');
 }
 
 SocialNetwork.prototype.btnSignOutClick = function(e) {
 	socialNetwork.justSignedOut = true;
-	gapi.auth.signOut();
+
+	if (platform == PLATFORM_GOOGLE) {
+		gapi.auth.signOut();
+	} else if (platform == PLATFORM_FACEBOOK) {
+		FB.api('/me/permissions', 'DELETE', function(response) {
+			document.location.href = "/";
+		});
+	}
 }
 
 SocialNetwork.prototype.retrieveFriends = function(callback) {
@@ -177,6 +153,105 @@ SocialNetwork.prototype.retrieveFriends = function(callback) {
 }
 
 function gPlusSigninCallback(authResult) {
-	socialNetwork.signinCallback(authResult);
+	if (authResult['access_token']) {
+		platform = PLATFORM_GOOGLE;
+		$("#signin-wrapper").css("display", "none");
+
+		// Check to see if user has at least 10 friends
+		gapi.client.load('plus', 'v1', function() {
+			var request = gapi.client.plus.people.list({
+				'userId' : 'me',
+				'collection' : 'visible',
+				'orderBy' : 'best'
+			});
+			request.execute(function(resp) {
+				if (resp.totalItems >= 10) {
+					$("#btn-play").css("display", "block");
+				} else {
+					$("#msg-error").css("display", "block");
+				}
+			});
+		});
+
+		gapi.client.load('plus', 'v1', function() {
+			var request = gapi.client.plus.people.get({
+				'userId' : 'me'
+			});
+			request.execute(function(resp) {
+				var url = resp.image.url;
+				url = url.substring(0, url.length - 2);
+				url += "30";
+				socialNetwork.displayUser(resp.displayName, url);
+			});
+		});
+
+		randomizeGender();
+	} else if (authResult['error']) {
+		if (socialNetwork.justSignedOut) {
+			document.location.href = "/";
+		} else if (platform != PLATFORM_FACEBOOK) {
+			$("#signin-wrapper").css("display", "block");
+		}
+	}
 }
-	
+
+window.fbAsyncInit = function() {
+	FB.init({
+		appId : '606653572730623',
+		status : true,
+		cookie : true,
+		xfbml : true
+	});
+
+	$("#signin-facebook").click(signinFacebookClick);
+
+	FB.Event.subscribe('auth.authResponseChange', function(response) {
+		// Here we specify what we do with the response anytime this event
+		// occurs.
+		if (response.status === 'connected') {
+			// The response object is returned with a status field that lets the
+			// app know the current
+			// login status of the person. In this case, we're handling the
+			// situation where they
+			// have logged in to the app.
+			platform = PLATFORM_FACEBOOK;
+
+			$("#signin-wrapper").css("display", "none");
+			$("#btn-play").css("display", "block");
+			FB.api('/me', function(response) {
+				var url = "https://graph.facebook.com/" + response.id
+						+ "/picture?width=30&height=30";
+				socialNetwork.displayUser(response.name, url);
+			});
+			randomizeGender();
+		} else if (response.status === 'not_authorized') {
+			// In this case, the person is logged into Facebook, but not into
+			// the app, so we call
+			// FB.login() to prompt them to do so.
+			// In real-life usage, you wouldn't want to immediately prompt
+			// someone to login
+			// like this, for two reasons:
+			// (1) JavaScript created popup windows are blocked by most browsers
+			// unless they
+			// result from direct interaction from people using the app (such as
+			// a mouse click)
+			// (2) it is a bad experience to be continually prompted to login
+			// upon page load.
+			$("#signin-wrapper").css("display", "block");
+		} else {
+			// In this case, the person is not logged into Facebook, so we call
+			// the login()
+			// function to prompt them to do so. Note that at this stage there
+			// is no indication
+			// of whether they are logged into the app. If they aren't then
+			// they'll see the Login
+			// dialog right after they log in to Facebook.
+			// The same caveats as above apply to the FB.login() call here.
+			$("#signin-wrapper").css("display", "block");
+		}
+	});
+}
+
+function signinFacebookClick() {
+	FB.login();
+}
